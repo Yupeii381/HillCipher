606038e0b27b72a603684e23078c3b4c351a6a42
Yupeii381 <vovabulaev5@gmail.com>
2025-10-16 19:16:52 +0700

    [NEW] Hasher

---
+++ HillCipher/Services/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace HillCipher.Services
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
---
+++ HillCipher/Services/PasswordHasher.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Security.Cryptography;
+
+namespace HillCipher.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        const int SaltLength = 16;
+        const int HashIterations = 100000;
+        const int HashLength = 32;
+
+        public string HashPassword(string password)
+        {
+            byte[] salt = GenerateSalt();
+            byte[] hash = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: HashIterations,
+                numBytesRequested: HashLength
+             );
+
+            byte[] hashBytes = new byte[SaltLength + HashLength];
+            Array.Copy(salt, 0, hashBytes, 0, SaltLength);
+            Array.Copy(hash, 0, hashBytes, SaltLength, HashLength);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+        
+        private byte[] GenerateSalt() => RandomNumberGenerator.GetBytes(SaltLength);
+
+        public bool VerifyPassword(string password, )
+    }
+}
