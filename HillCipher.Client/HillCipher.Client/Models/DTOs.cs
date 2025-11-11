29810445f84c7ceb18a5448d05d55469ca437f1a
Yupeii381 <vovabulaev5@gmail.com>
2025-11-11 08:00:27 +0700

    This branch is server-only.

--- HillCipher.Client/HillCipher.Client/Models/DTOs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HillCipher.Client.Models;
-
-public class TextDto
-{
-    public Guid Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Content { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-}
